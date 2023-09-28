bb8fe776eff695086f8317352c0bbbd9dd867b1b
Higor Santos <higor.stos@outlook.com>
2023-09-27 22:13:26 -0300

    feat: redux

---
+++ src/store/index.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import api from '../services/api'
+
+import carrinhoReducer from './reducers/carrinho'
+import favoritosReducer from './reducers/favoritos'
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    carrinho: carrinhoReducer,
+    favoritos: favoritosReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
